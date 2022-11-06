f7ac0498389b71bfe7d8c4869d39fd907b8184d6
Ajay <ajaybhoite8230@gmail.com>
2022-11-06 21:47:41 +0530

    Commit to add new Test Case

---
+++ src/nopCommerce_D001/TC_Delete_Customer_D001.java
@@ -0,0 +1,10 @@
+package nopCommerce_D001;
+
+public class TC_Delete_Customer_D001 {
+
+	public static void main(String[] args) {
+		// This will delete the some customer details.
+
+	}
+
+}
---
+++ src/nopCommerce_D001/TC_Serch_Customer_D001.java
@@ -0,0 +1,10 @@
+package nopCommerce_D001;
+
+public class TC_Serch_Customer_D001 {
+
+	public static void main(String[] args) {
+		// Test for customer
+
+	}
+
+}
